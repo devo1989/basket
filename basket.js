05224b96011622cb5c6bd7fbfeb7c774b90e38ee
devo1989 <43476602+devo1989@users.noreply.github.com>
2019-11-13 00:22:32 +0000

    added template render functions

--- basket.js
+++ basket.js
@@ -43,6 +43,9 @@ class Product {
 
 var basket = [];
 const products = rawProducts.map(product => new Product(product));
+document.addEventListener("DOMContentLoaded", function() { 
+    renderProducts();
+});
 
 //** Basket functions **//
 
@@ -64,6 +67,17 @@ function removeItem(product) {
     calculateTotal();
 }
 
+//event listened for product quantity update.
+function editBasketItemQuantity(product, quantity) {
+    const value = parseInt(quantity);
+    if (value == 0) {
+        removeItem(product);
+    } else {
+        product.setQuantity(value)
+        calculateTotal();
+    }
+}
+
 function emptyBasket() {
     basket = [];
     calculateTotal();
@@ -77,4 +91,48 @@ function calculateTotal() {
     //round our cost to 2 decimal places.
     currentCost = currentCost.toFixed(2);
     renderBasket(currentCost);
+}
+
+/*** OUR TEMPLATE RENDER FUNCTIONS ***/
+
+//iterates over our products creating a div block for each.
+function renderProducts() {
+    const template = document.getElementById('product-item');
+    const list = document.getElementById('product-list');
+
+    products.forEach(product => {
+        const clone = document.importNode(template.content, true);
+        const div = clone.childNodes[1];
+        const childNodes = div.children;
+
+        childNodes[0].innerHTML = product.name;
+        childNodes[1].innerHTML = "cost - " + "£" + product.price;
+        childNodes[2].onclick = function() { addItem(product) };
+        list.appendChild(clone);
+    })
+}
+
+//iterates over the products in our basket and renders them out in a list with quantity and cost.
+function renderBasket(cost) {
+
+    //wipes our current basket.
+    const basketList = document.getElementById("basket-list");
+    while(basketList.firstChild) {
+        basketList.removeChild(basketList.firstChild);
+    }
+
+    const template = document.getElementById('basket-item');
+    const list = document.getElementById('basket-list');
+
+    basket.forEach(product => {
+        const clone = document.importNode(template.content, true);
+        const childNodes = clone.children;
+
+        childNodes[0].innerHTML = product.name;
+        childNodes[2].value = product.quantity;
+        childNodes[2].addEventListener('input', function() { editBasketItemQuantity(product, this.value) });
+        list.appendChild(clone);
+    });
+    const total = document.getElementById('total');
+    total.innerHTML = 'Total Cost: £' + cost;
 }
